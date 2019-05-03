288cc3ae494359b288275f1ab8fb71d3baad8d3b
Kazutaka Homma <special18fender@gmail.com>
2019-05-02 23:07:33 -0700

    Added data models

---
+++ Motivate Me CP/Model/Quote+CoreDataProperties.swift
@@ -0,0 +1,44 @@
+//
+//  Quote+CoreDataProperties.swift
+//  Motivate Me CP
+//
+//  Created by Kazutaka Homma on 5/2/19.
+//  Copyright © 2019 Kazutaka Homma. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Quote {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Quote> {
+        return NSFetchRequest<Quote>(entityName: "Quote")
+    }
+
+    @NSManaged public var id: Int64
+    @NSManaged public var text: String?
+    @NSManaged public var topic: String?
+    @NSManaged public var isFavorited: Bool
+    @NSManaged public var source: Source?
+    @NSManaged public var tags: NSSet?
+
+}
+
+// MARK: Generated accessors for tags
+extension Quote {
+
+    @objc(addTagsObject:)
+    @NSManaged public func addToTags(_ value: Tag)
+
+    @objc(removeTagsObject:)
+    @NSManaged public func removeFromTags(_ value: Tag)
+
+    @objc(addTags:)
+    @NSManaged public func addToTags(_ values: NSSet)
+
+    @objc(removeTags:)
+    @NSManaged public func removeFromTags(_ values: NSSet)
+
+}
